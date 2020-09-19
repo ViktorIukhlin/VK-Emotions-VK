1a2f2a462c1b4bc1dd2fa2512b464ad1cb947618
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-09-19 11:36:10 +0300

    Updates

---
+++ precache-manifest.5a2cb5187eba9b6de37443aa0dbd55df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4306eece5367db7f88709546ac1b70d2",
+    "url": "/VK-Emotions-VK/index.html"
+  },
+  {
+    "revision": "bde2bbf3ef6850d924b5",
+    "url": "/VK-Emotions-VK/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "12a1a3b6f7c2f117a166",
+    "url": "/VK-Emotions-VK/static/css/main.570dbad6.chunk.css"
+  },
+  {
+    "revision": "bde2bbf3ef6850d924b5",
+    "url": "/VK-Emotions-VK/static/js/2.896368b7.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/VK-Emotions-VK/static/js/2.896368b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a1a3b6f7c2f117a166",
+    "url": "/VK-Emotions-VK/static/js/main.0826cf1f.chunk.js"
+  },
+  {
+    "revision": "81a52e209567dcdc90e4",
+    "url": "/VK-Emotions-VK/static/js/runtime-main.b5a84b93.js"
+  },
+  {
+    "revision": "e7f22b8f1fae16ea9f4c2e04b540b691",
+    "url": "/VK-Emotions-VK/static/media/blue.e7f22b8f.jpg"
+  }
+]);
